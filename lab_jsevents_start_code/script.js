7ea6f65563abc03a2d3b4c9766952611d938503d
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-24 16:01:03 +0100

    add functionality to mark item as complete, and move them to a completed task list

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -3,6 +3,7 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector('#new-todo');
 const todoList = document.querySelector('#list');
 const showDateButton = document.querySelector('#show-date');
+const completedList = document.querySelector('#completed-list');
 
 
 //logic
@@ -10,17 +11,34 @@ todoForm.addEventListener('submit', (event) =>{
     event.preventDefault();
     const newTodoItem = document.createElement('li');
 
+    const completeButton = document.createElement('button')
+    completeButton.textContent = 'complete';
+
     const deleteButton = document.createElement('button')
     deleteButton.textContent = 'delete';
+
     deleteButton.addEventListener('click', () => {
-    todoList.removeChild(newTodoItem);
-    todoList.removeChild(deleteButton);
+        todoList.removeChild(newTodoItem);
+        todoList.removeChild(completeButton);
+        todoList.removeChild(deleteButton);
     });
+
+    completeButton.addEventListener('click', () => {
+        newTodoItem.style.color = 'gray';
+        newTodoItem.style.textDecoration = 'line-through';
+
+        completedList.appendChild(newTodoItem);
+        todoList.removeChild(completeButton);
+        todoList.removeChild(deleteButton);
+    });
+
 
     newTodoItem.innerText = event.target["new-todo"].value;
     todoList.appendChild(newTodoItem);
+    todoList.appendChild(completeButton);
     todoList.appendChild(deleteButton);
 });
+
 
 showDateButton.addEventListener('click', () =>{
     const currentDate = new Date().toLocaleDateString();
