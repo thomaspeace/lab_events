e83c6c0a4740be1dd89654be483dea04b3fe41db
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-24 14:45:32 +0100

    add button to delete on list

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,11 +1,20 @@
 //selectors
 const todoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector('#list');
+// const deleteButon = document.createElement('button');
 
 //logic
 todoForm.addEventListener('submit', (event) =>{
     event.preventDefault();
     const newTodoItem = document.createElement('li');
-    newTodoItem.innerText = event.target["new-todo"].value
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'delete';
+    newTodoItem.innerText = event.target["new-todo"].value;
     todoList.appendChild(newTodoItem);
+    todoList.appendChild(deleteButton);
 })
+
+const deleteButton = document.createElement('button')
+deleteButton.addEventListener('click', () => {
+    console.log("asdasdas");;
+});
