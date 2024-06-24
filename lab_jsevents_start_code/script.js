e6c8dc0045b64f3426f2599bbd359accde28cd17
Aaron Ayeni <aaronayeni135@gmail.com>
2024-06-24 15:06:02 +0100

    created functioning delete button for each element

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,20 +1,25 @@
 //selectors
 const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector('#new-todo');
 const todoList = document.querySelector('#list');
-// const deleteButon = document.createElement('button');
+
 
 //logic
 todoForm.addEventListener('submit', (event) =>{
     event.preventDefault();
     const newTodoItem = document.createElement('li');
+
     const deleteButton = document.createElement('button')
     deleteButton.textContent = 'delete';
+    deleteButton.addEventListener('click', () => {
+    todoList.removeChild(newTodoItem);
+    todoList.removeChild(deleteButton);
+    });
+
     newTodoItem.innerText = event.target["new-todo"].value;
     todoList.appendChild(newTodoItem);
     todoList.appendChild(deleteButton);
-})
-
-const deleteButton = document.createElement('button')
-todoList.addEventListener('click', () => {
-    todoList.removeChild(this.todoList);
 });
+
+
+
